ddba959bcb15a35fc5e9ffe66db22ba001a39167
RealPotatoe <bengel_moritz@gmx.de>
2025-12-13 14:03:07 +0100

    Setup day06

---
+++ day06/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Advent of Code Day 06 - Kotlin";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = pkgs.writeShellScriptBin "day06" ''
+          ${pkgs.kotlin}/bin/kotlin ${./main.kt}
+        '';
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.default;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            kotlin
+            jdk
+            kotlin-language-server
+            gnumake
+          ];
+        };
+      }
+    );
+}
